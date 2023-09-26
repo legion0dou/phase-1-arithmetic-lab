2f2a434ea8c6385bb3a6a0a449df77e636e335f4
user_name <email_id>
2023-09-26 12:04:25 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
 
 // Write your code here
+// Task 1: Create a variable called multiply
+// Set it to an equation that will multiply the variables num1 and num2; 
+// the result of the multiplication should be 62.
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+// Task 2: Create a variable called random
+// Generate a random integer greater than 0.
+const random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+
+// Task 3: Create a variable called mod
+// Set it to an equation that will calculate the remainder of dividing 
+// variable num3 by num4; the remainder should be 4.
+const num3 = 40;
+const num4 = 36;
+const mod = num3 % num4;
+
+// Task 4: Create a variable called max
+// Find the highest number in a set; the value returned should be 20.
+const max = Math.max(5, 10, 15, 20);
+
+// Testing the results
+console.log("Multiply:", multiply); // Output: 62
+console.log("Random:", random);     // Output: A random integer greater than 0
+console.log("Mod:", mod);           // Output: 4
+console.log("Max:", max);           // Output: 20
